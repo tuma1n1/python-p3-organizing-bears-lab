ce64ca3333d0ec4b1e468c31dbc7bf9097c8cc18
Tumaini <tumainikebaya.234@gmail.com>
2024-08-07 00:03:08 +0300

    Adds querries

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+('Mr. Chocolate', 10, 'M', 'brown', 'calm', 1),
+('Rowdy', 5, 'M', 'black', 'aggressive', 1),
+('Tabitha', 7, 'F', 'black', 'playful', 1),
+('Sergeant Brown', 12, 'M', 'brown', 'stern', 1),
+('Melissa', 8, 'F', 'brown', 'calm', 1),
+('Grinch', 6, 'M', 'black', 'grumpy', 1),
+('Wendy', 4, 'F', 'white', 'friendly', 1),
+(NULL, 3, 'F', 'brown', 'curious', 1);
